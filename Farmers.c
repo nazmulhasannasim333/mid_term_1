9f5e5177cc1b42cd9c3301dcbda6986e90f1427d
Nasim <mdnasimhosen333@gmail.com>
2025-04-25 12:11:31 +0600

    solved problem 3

--- Farmers.c
+++ Farmers.c
@@ -4,10 +4,15 @@ int main()
 {
     int t;
     scanf("%d", &t);
-    int m1, m2;
-    scanf("%d %d", &m1, &m2);
-    int d;
-    scanf("%d", &d);
+    for (int i = 0; i < t; i++)
+    {
+        int m1, m2, d;
+        scanf("%d %d %d", &m1, &m2, &d);
+        int totalFarmers = m1 + m2;
+        int newDays = d * m1 / totalFarmers;
+        int fewerDays = d - newDays;
+        printf("%d\n", fewerDays);
+    }
 
     return 0;
 }
